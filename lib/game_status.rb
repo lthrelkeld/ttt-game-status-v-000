f69b46b49beb35ccf1525bdecaebd7c72801f30e
Elizabeth Karst <lkarst@outlook.com>
2018-08-11 20:42:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,6 @@ WIN_COMBINATIONS = [
 
 #determines if win is true or false
 def won?(board)
-  res = false
-
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]

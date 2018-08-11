259f820203d340f1a0b705819df77c6d730233e9
Elizabeth Karst <lkarst@outlook.com>
2018-08-11 20:36:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,15 +23,14 @@ def won?(board)
     win_index_3 = win_combination[2]
 
     if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
-      res = [board[win_index_1],board[win_index_2],board[win_index_3]]
+      [board[win_index_1],board[win_index_2],board[win_index_3]]
     elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
-      res = [board[win_index_1],board[win_index_2],board[win_index_3]]
+      [board[win_index_1],board[win_index_2],board[win_index_3]]
     else
-      res = false
+      false
     end
 #    puts board[win_combination] if board[win_combination] == ["X","X","X"] || board[win_combination] == ["O","O","O"]
   end
-  res
 end
 
 def full?(board)

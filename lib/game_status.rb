8fc21f78a4f6a940d8c35e0697a558893ba5f3b8
Elizabeth Karst <lkarst@outlook.com>
2018-08-11 19:55:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #Top row
+  [3,4,5], #Middle Row
+  [6,7,8], #Bottom row
+  [0,3,6], #first columns
+  [1,4,7], #second columns
+  [2,5,8], #last column
+  [0,4,8], #first diagonal
+  [2,4,6] #second diagonal
+]
